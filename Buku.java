e49366f80f700f3a0f1623d096df27d926faa7e4
Rafi Abyantara Pratama <rafiabyantarapratama@gmail.com>
2025-04-15 22:32:17 +0700

    Rafi_buku

--- Buku.java
+++ Buku.java
@@ -1,6 +1,24 @@
 package KolebV2;
 
 public class Buku {
-
+   
+        String kodeBuku, judul, penulis;
+        int tahunTerbit;
     
-}
+        public Buku(String kode, String judul, String penulis, int tahun) {
+            this.kodeBuku = kode;
+            this.judul = judul;
+            this.penulis = penulis;
+            this.tahunTerbit = tahun;
+        }
+    
+        public void tampilInformasi() {
+            System.out.println("Kode Buku: " + kodeBuku);
+            System.out.println("Judul: " + judul);
+            System.out.println("Penulis: " + penulis);
+            System.out.println("Tahun Terbit: " + tahunTerbit);
+            System.out.println("------------------------");
+        }
+    }
+    
+    
